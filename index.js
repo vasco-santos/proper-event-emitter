1db035f8c06888799a0830637694ebf326d29d2e
Vasco Santos <vasco.santos@moxy.studio>
2020-09-28 11:42:50 +0200

    feat: initial implementation

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const { EventEmitter } = require('events')
+
+class ProperEventEmitter extends EventEmitter {
+  /**
+   * Synchronously calls each of the listeners registered for the event named eventName,
+   * in the order they were registered, passing the supplied arguments to each.
+   * @param {string} eventName
+   * @param {*} arg
+   * @returns {boolean}
+   */
+  emit (eventName, ...args) {
+    let r = false
+    try {
+      r = super.emit(eventName, ...args)
+    } catch (err) {
+      // Throw global context error if handler throws
+      setTimeout(() => { throw err }, 1)
+    }
+
+    return r
+  }
+}
+
+module.exports = ProperEventEmitter
